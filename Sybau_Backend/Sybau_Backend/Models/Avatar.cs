aa12f8a396659e0efd79d91267887e205b3b8d8b
bakirdrc <dur220140@spengergasse.at>
2025-12-25 18:45:15 +0100

    Avatar und BaseEntity added

---
+++ Sybau_Backend/Sybau_Backend/Models/Avatar.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sybau_Backend.Models;
+
+public class Avatar:BaseEntity<int>
+{
+    [Required]
+    public string Name { get; set; }
+    
+    public int Level { get; set; }
+    
+    public int Experience { get; set; }
+}
---
+++ Sybau_Backend/Sybau_Backend/Models/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Sybau_Backend.Models;
+
+public abstract class BaseEntity<T> where T: struct
+{
+    public T Id { get;protected set; }
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime UpdatedAt { get; protected set; }
+}
