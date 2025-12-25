c9ec640fb35bdadbd9b907a27afc1001641ef84e
bakirdrc <dur220140@spengergasse.at>
2025-12-25 18:44:36 +0100

    dbcontext bearbeitet

--- Sybau_Backend/Sybau_Backend/Data/FitnessDbContext.cs
+++ Sybau_Backend/Sybau_Backend/Data/FitnessDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace Sybau_Backend.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Sybau_Backend.Models;
 
-public class FitnessDbContext
+namespace Sybau_Backend.Data;
+
+public class FitnessDbContext:DbContext
 {
-    
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Avatar> Avatars => Set<Avatar>();
+    public FitnessDbContext(DbContextOptions<FitnessDbContext> options) : base(options){}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(builder =>
+        {
+            builder.HasOne(p => p.Avatar)
+                .WithOne()
+                .HasForeignKey<Avatar>("UserId");
+        });
+    }
 }
