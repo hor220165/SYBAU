899955be15e9ad624838e298572b1b4d726bc35e
bakirdrc <dur220140@spengergasse.at>
2025-12-25 17:46:37 +0100

    User updated

--- Sybau_Backend/Sybau_Backend/Models/User.cs
+++ Sybau_Backend/Sybau_Backend/Models/User.cs
@@ -1,10 +1,37 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace Sybau_Backend.Models;
 
 public class User
 {
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    [Key] public int Id { get; set; }
+    [Required] public required string FirstName { get; set; }
+    [Required] public required string LastName { get; set; }
+    [Required] [EmailAddress] public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    protected User()
+    {
+    }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+    public User(string firstName, string lastName, string email, string password)
+    {
+        if (string.IsNullOrWhiteSpace(firstName)) throw new ArgumentNullException(nameof(firstName));
+        if (string.IsNullOrWhiteSpace(lastName)) throw new ArgumentNullException(nameof(lastName));
+        if (string.IsNullOrWhiteSpace(email)) throw new ArgumentNullException(nameof(email));
+        if(string.IsNullOrWhiteSpace(password)) throw new ArgumentNullException(nameof(password));
+        
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+       
+    }
 }
