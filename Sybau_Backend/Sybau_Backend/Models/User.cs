207b0cdf960d52cd6ec3fe2bfc40f2d890bb2fc1
bakirdrc <dur220140@spengergasse.at>
2025-12-25 22:03:52 +0100

    updated

--- Sybau_Backend/Sybau_Backend/Models/User.cs
+++ Sybau_Backend/Sybau_Backend/Models/User.cs
@@ -6,22 +6,13 @@ namespace Sybau_Backend.Models;
 
 public class User: BaseEntity<int>
 {
-    [Required] public required string FirstName { get; set; }
-    [Required] public required string LastName { get; set; }
-    [Required] [EmailAddress] public string Email { get; set; }
-    [Required] public Avatar Avatar { get; set; }
-
-    [Required]
-    [DataType(DataType.Password)]
-    public string Password { get; set; }
-
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     protected User()
     {
     }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
-    public User(string firstName, string lastName, string email, string password): base()
+    public User(string firstName, string lastName, string email, string password, Avatar avatar)
     {
         if (string.IsNullOrWhiteSpace(firstName)) throw new ArgumentNullException(nameof(firstName));
         if (string.IsNullOrWhiteSpace(lastName)) throw new ArgumentNullException(nameof(lastName));
@@ -32,6 +23,18 @@ public class User: BaseEntity<int>
         LastName = lastName;
         Email = email;
         Password = password;
-       
+        Avatar = avatar;
     }
+    
+    [Required] public required string FirstName { get; set; }
+    [Required] public required string LastName { get; set; }
+    [Required] [EmailAddress] public string Email { get; set; }
+    [Required] public Avatar Avatar { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    
+    public ICollection<UserItem> UserItems { get; set; } = new List<UserItem>();
+    public ICollection<UserChallenge> UserChallenges { get; set; } = new List<UserChallenge>();
 }
