893cf05808f4e9f7144d4dee91921d126812f0d4
bakirdrc <dur220140@spengergasse.at>
2025-12-25 22:02:28 +0100

    Challenge und UserChallenge added

---
+++ Sybau_Backend/Sybau_Backend/Models/Challenge.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace Sybau_Backend.Models;
+
+public class Challenge : BaseEntity<int>
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    protected Challenge() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+    public Challenge(string name, string description, int xpReward)
+    {
+        if(string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name));
+        if(string.IsNullOrWhiteSpace(description)) throw new ArgumentNullException(nameof(description));
+        if(xpReward < 1) throw new ArgumentOutOfRangeException(nameof(xpReward));
+        Name = name;
+        Description = description;
+        XpReward = xpReward;
+    }
+    
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public int XpReward { get; set; }
+    
+    public ICollection<UserChallenge> UserChallenges { get; set; } = new List<UserChallenge>();
+}
---
+++ Sybau_Backend/Sybau_Backend/Models/UserChallenge.cs
@@ -0,0 +1,27 @@
+﻿namespace Sybau_Backend.Models;
+
+public class UserChallenge: BaseEntity<int>
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    protected UserChallenge() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+    public UserChallenge(User user, Challenge challenge)
+    {
+        User = user;
+        Challenge = challenge;
+        Progress = 0;
+    }
+    
+    public User User { get; set; }
+    public Challenge Challenge { get; set; }
+    public int Progress { get; set; }
+    public bool Completed
+    {
+        get => Progress >= 100;
+    }
+
+
+    public DateTime? CompletedAt { get; set; }
+    
+}
