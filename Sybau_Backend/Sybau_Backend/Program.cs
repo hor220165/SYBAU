6f7fe48cad96b8e500ad457dc2e910dbebd16250
bakirdrc <dur220140@spengergasse.at>
2025-12-25 22:03:33 +0100

    Sqlite db erstellt

--- Sybau_Backend/Sybau_Backend/Program.cs
+++ Sybau_Backend/Sybau_Backend/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Sybau_Backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<FitnessDbContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
