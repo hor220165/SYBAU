daea9b07dd666265828deeb6ef15f46778c5d3f9
bakirdrc <dur220140@spengergasse.at>
2025-12-26 20:57:27 +0100

    Property zu User added

---
+++ Sybau_Backend/Sybau_Backend/Migrations/20251226184106_IsAdminAddedToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sybau_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsAdminAddedToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isAdmin",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isAdmin",
+                table: "Users");
+        }
+    }
+}
