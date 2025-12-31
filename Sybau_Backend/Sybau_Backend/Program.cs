600884d92f766b0d8cbd48301702175744178ef3
bakirdrc <dur220140@spengergasse.at>
2026-01-01 00:28:52 +0100

    JsonConverter added

--- Sybau_Backend/Sybau_Backend/Program.cs
+++ Sybau_Backend/Sybau_Backend/Program.cs
@@ -8,8 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<ChallengeService>();
+builder.Services.AddScoped<ShopService>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(
+        new System.Text.Json.Serialization.JsonStringEnumConverter()
+    );
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddDbContext<FitnessDbContext>(options =>
     options.UseSqlite(
