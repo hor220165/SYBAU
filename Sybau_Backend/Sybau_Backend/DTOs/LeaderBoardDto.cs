b1407b3f9824f82cd6ecb24fff8cef796b43e5d5
bakirdrc <dur220140@spengergasse.at>
2026-02-03 02:55:49 +0100

    LeaderboardDto added

---
+++ Sybau_Backend/Sybau_Backend/DTOs/LeaderBoardDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Sybau_Backend.DTOs;
+
+public class LeaderBoardDto
+{
+    public int Id { get; set; }
+    public int Rank { get; set; }
+    public string UserName { get; set; }
+    public int Experience { get; set; }
+    public int Level { get; set; }
+}
