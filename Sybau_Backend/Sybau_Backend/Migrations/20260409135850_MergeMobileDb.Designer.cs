bccd3f49ecf5cac0b3fdb008db382504e2b73f96
David Pfeiffer <pfe220214@spengergasse.at>
2026-04-09 15:59:07 +0200

    Add MergeMobileDb migration

--- Sybau_Backend/Sybau_Backend/Migrations/20260409135850_MergeMobileDb.Designer.cs
+++ Sybau_Backend/Sybau_Backend/Migrations/20260409135850_MergeMobileDb.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Sybau_Backend.Data;
@@ -11,9 +12,11 @@ using Sybau_Backend.Data;
 namespace Sybau_Backend.Migrations
 {
     [DbContext(typeof(FitnessDbContext))]
-    partial class FitnessDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260409135850_MergeMobileDb")]
+    partial class MergeMobileDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
